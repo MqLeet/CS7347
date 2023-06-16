8a460006654efb50b701d2ec36ae1c7c9a2cbf02
Mqleet <Mqleet@gmail.com>
2023-06-16 17:59:00 +0800

    delete some files

--- train_bert.sh
+++
@@ -1,7 +0,0 @@
-export CUDA_VISIBLE_DEVICES=1
-python train_bert_base.py --exp_desc "only_TNLI" \
-                          --do_adv 0 \
-                          --lr 5e-5 \
-                          --epochs 10 \
-                          --batch_size 64 \
-                          --adv_train_type "fgm"
